a0eaec78446f9da1bba0f4d035feaacef76d6bfa
Richard Förster <richard.foerster@ivi.fraunhofer.de>
2022-05-07 12:18:21 +0200

    Youtube Downloader Prototyp

--- Download/Download.cs
+++ Download/Download.cs
@@ -3,5 +3,27 @@
 namespace Project.Download
 {
 
+    public interface IDownloader
+    {
+
+        string SourceName { get; }
+
+        void Download(string source, string destination);
+
+    }
+
+
+    public class YoutubeDownloader : IDownloader
+    {   
+
+        public static readonly YoutubeDownloader Instance = new();
+
+        public string SourceName { get => "YouTube"; }
+
+        public void Download(string source, string destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
 
 }
