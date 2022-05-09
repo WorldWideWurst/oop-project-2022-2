907ef01ba3a8894b312552bfc6272853c4ceb319
Richard Förster <richard.foerster@ivi.fraunhofer.de>
2022-05-09 21:12:49 +0200

    Converter Prototyp

---
+++ Convert/Converter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Convert
+{
+    public interface IConverter
+    {
+
+        bool SupportsConversion(string sourceExtension, string targetExtension);
+
+        void Convert(string source, string target);
+    }
+
+    public class Converter : IConverter
+    {
+
+        public static readonly Converter Instance = new Converter();
+
+        public void Convert(string source, string target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SupportsConversion(string sourceExtension, string targetExtension)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
