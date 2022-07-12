ca06cb43340c41ed6e238503e0b8d4d186bba522
Richard Förster <richard.foerster@ivi.fraunhofer.de>
2022-07-13 01:08:00 +0200

    Windows Explorer wird nun geöffnet, wenn man den richtigen Button klickt.

--- UI/MVVM/View/LibraryPages/SourceOverview.xaml.cs
+++ UI/MVVM/View/LibraryPages/SourceOverview.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,7 +41,7 @@ namespace Project.UI.MVVM.View.LibraryPages
 
         private void ViewSourceInExplorer(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Hier öffnet sich nun der Explorer TODO");
+            Process.Start("explorer.exe", $"\"{Directory.GetParent(((SourceViewModel)DataContext).Source.Address)}\"");
         }
 
         private void ViewMusic(object sender, RoutedEventArgs e)
